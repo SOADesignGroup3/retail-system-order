81e2c3b3320f575ab1c093e9bcef9caf202ebfb0
timadigwe <timothyadigwe2019@gmail.com>
2023-08-13 06:21:16 +0100

    order service implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,6 @@ import { OrderModule } from './order/order.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      //host: 'localhost',
-      //port: 5432,
-      //database: 'micro_order',
       url: 'postgres://cunkhifa:slN8vP_ru-SZJlqNzjxVGoW59IuF6DMF@trumpet.db.elephantsql.com/cunkhifa',
       username: 'cunkhifa',
       password: 'slN8vP_ru-SZJlqNzjxVGoW59IuF6DMF',
